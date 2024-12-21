d51a2da67e24d6526b6729c5d8fce875d4720f0c
Oleh <122808904+opoiasnik@users.noreply.github.com>
2024-12-21 19:01:56 +0100

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { PostsModule } from './posts/posts.module';
       database: 'railway',
       autoLoadEntities: true,
       logging: true,
+      synchronize: true
     }),
     PostsModule,
   ],
