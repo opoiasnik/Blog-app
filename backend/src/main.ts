d2500937f1eb2d7c18a443b4f76c7e8d08cbe703
Oleh <122808904+opoiasnik@users.noreply.github.com>
2024-12-21 19:32:54 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://luminous-semifreddo-9fdb6d.netlify.app/',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
